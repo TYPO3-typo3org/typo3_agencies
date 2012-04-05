2934fa16cca20493c632aa460ef90225af882fd3
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-05 11:21:45 +0000

    #35711 - fixed JS inclusion issue when the plugin is not cacheable

--- Classes/ViewHelpers/ScriptViewHelper.php
+++ Classes/ViewHelpers/ScriptViewHelper.php
@@ -29,6 +29,41 @@
  */
 class Tx_Typo3Agencies_ViewHelpers_ScriptViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
+	/**
+	 * @var Tx_Extbase_Configuration_ConfigurationManagerInterface
+	 */
+	protected $configurationManager;
+	
+	/**
+	 * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
+	 * @return void
+	 */
+	public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager) {
+		$this->configurationManager = $configurationManager;
+	}
+
+	/**
+	 * @var t3lib_PageRenderer
+	 */
+	protected $pageRenderer;
+	
+	/**
+	 * @param t3lib_PageRenderer $pageRenderer
+	 */
+	public function injectPageRenderer(t3lib_PageRenderer $pageRenderer) {
+		$this->pageRenderer = $pageRenderer;
+	}	
+	
+	/**
+	 * Returns TRUE if what we are outputting may be cached
+	 *
+	 * @return boolean
+	 */
+	protected function isCached() {
+		$userObjType = $this->configurationManager->getContentObject()->getUserObjectType();
+		return ($userObjType !== tslib_cObj::OBJECTTYPE_USER_INT);
+	}
+	
     /**
      * @param string $content
      * @param bool $inline
@@ -36,11 +71,16 @@ class Tx_Typo3Agencies_ViewHelpers_ScriptViewHelper extends Tx_Fluid_Core_ViewHe
      * @param bool $forceOnTop
      * @return void
      */
-	public function render($inline=TRUE, $compress=TRUE, $forceOnTop=FALSE) {
+	public function render($compress=TRUE, $forceOnTop=FALSE) {
         $content = $this->renderChildren();
-        /** @var $pagerender t3lib_pagerenderer */
-        $pagerender = $GLOBALS['TSFE']->getPageRenderer();
-        $pagerender->addJsInlineCode(md5($content), $content, $compress, $forceOnTop);
+        
+        if ($this->isCached()) {
+        	$this->pageRenderer->addJsFooterInlineCode(md5($content), $content, $compress, $forceOnTop);
+        } else {
+        	// additionalFooterData not possible in USER_INT
+        	$GLOBALS['TSFE']->additionalHeaderData[md5($content)] = t3lib_div::wrapJS($content);
+        }
+        
 	}
 }
 ?>
